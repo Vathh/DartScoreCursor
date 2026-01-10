f2b444083564981c3984eb4e18ed13ff3679f93e
Maciej Przestrzelski <przestrzelski.maciej1@gmail.com>
2026-01-10 19:53:40 +0100

    CD : Pracuje nad logika przyznawania punktow za udzialy w turniejach.

--- app/Factories/TournamentResultsFactory.php
+++ app/Factories/TournamentResultsFactory.php
@@ -2,10 +2,15 @@
 
 namespace App\Factories;
 
+use App\Domain\GroupStandingDomain;
 use Illuminate\Support\Collection;
 
 class TournamentResultsFactory
 {
+    /**
+     * @param Collection<GroupStandingDomain> $groupStandings
+     * @return Collection
+     */
     public function createForGroups(Collection $groupStandings): Collection
     {
         $groupStandings->map(function ($group) {})
--- database/seeders/PointSchemeSeeder.php
+++ database/seeders/PointSchemeSeeder.php
@@ -11,24 +11,45 @@ class PointSchemeSeeder extends Seeder
 {
     public function run(): void
     {
-        $scheme = PointScheme::create([
+        $scheme25to32 = PointScheme::create([
             'name' => 'od 25 do 32 osob',
             'min_players' => 25,
             'max_players' => 32,
         ]);
 
         PointSchemeRule::insert([
-           ['scheme_id' => $scheme->id, 'elimination_stage' => EliminationStage::GROUP, 'place' => 4, 'points' => 2],
-           ['scheme_id' => $scheme->id, 'elimination_stage' => EliminationStage::GROUP, 'place' => 3, 'points' => 4],
-           ['scheme_id' => $scheme->id, 'elimination_stage' => EliminationStage::EIGHT, 'place' => null, 'points' => 7],
+           ['scheme_id' => $scheme25to32->id, 'elimination_stage' => EliminationStage::GROUP->value, 'place' => 4, 'points' => 2],
+           ['scheme_id' => $scheme25to32->id, 'elimination_stage' => EliminationStage::GROUP->value, 'place' => 3, 'points' => 4],
+           ['scheme_id' => $scheme25to32->id, 'elimination_stage' => EliminationStage::EIGHT->value, 'place' => null, 'points' => 7],
 
-           ['scheme_id' => $scheme->id, 'elimination_stage' => EliminationStage::QUARTER, 'place' => null, 'points' => 10],
+           ['scheme_id' => $scheme25to32->id, 'elimination_stage' => EliminationStage::QUARTER->value, 'place' => null, 'points' => 10],
 
-           ['scheme_id' => $scheme->id, 'elimination_stage' => EliminationStage::THIRD, 'place' => 4, 'points' => 14],
-           ['scheme_id' => $scheme->id, 'elimination_stage' => EliminationStage::THIRD, 'place' => 3, 'points' => 18],
+           ['scheme_id' => $scheme25to32->id, 'elimination_stage' => EliminationStage::THIRD->value, 'place' => 4, 'points' => 14],
+           ['scheme_id' => $scheme25to32->id, 'elimination_stage' => EliminationStage::THIRD->value, 'place' => 3, 'points' => 18],
 
-           ['scheme_id' => $scheme->id, 'elimination_stage' => EliminationStage::FINAL, 'place' => 2, 'points' => 22],
-           ['scheme_id' => $scheme->id, 'elimination_stage' => EliminationStage::FINAL, 'place' => 1, 'points' => 26],
+           ['scheme_id' => $scheme25to32->id, 'elimination_stage' => EliminationStage::FINAL->value, 'place' => 2, 'points' => 22],
+           ['scheme_id' => $scheme25to32->id, 'elimination_stage' => EliminationStage::FINAL->value, 'place' => 1, 'points' => 26],
+        ]);
+
+        $scheme32to39 = PointScheme::create([
+            'name' => 'od 32 do 39 osob',
+            'min_players' => 32,
+            'max_players' => 39,
+        ]);
+
+        PointSchemeRule::insert([
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::GROUP->value, 'place' => 5, 'points' => 2],
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::GROUP->value, 'place' => 4, 'points' => 4],
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::GROUP->value, 'place' => 3, 'points' => 7],
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::EIGHT->value, 'place' => null, 'points' => 10],
+
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::QUARTER->value, 'place' => null, 'points' => 14],
+
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::THIRD->value, 'place' => 4, 'points' => 18],
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::THIRD->value, 'place' => 3, 'points' => 22],
+
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::FINAL->value, 'place' => 2, 'points' => 26],
+            ['scheme_id' => $scheme32to39->id, 'elimination_stage' => EliminationStage::FINAL->value, 'place' => 1, 'points' => 30],
         ]);
     }
 }
